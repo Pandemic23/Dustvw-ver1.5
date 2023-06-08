47b07fcb12821cf504a6d044b765697152b54865
Pandemic23 <popofh12345@naver.com>
2023-06-09 01:06:43 +0900

    업데이트

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ from setuptools import setup, find_packages
 
 
 setup(name="Dustvw",
-    version="0.1.5",
+    version="0.1.7",
     author="Youngco",
     author_email="popofh12345@naver.com",
     description="Fine dust visualization",
     url="https://github.com/Pandemic23/FCV-ver1.5",
     install_requires=['Folium','pandas' ],
-    packages=['FCV'],
+    packages=['Dustvw'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
